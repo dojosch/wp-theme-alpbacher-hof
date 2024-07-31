629e06ed6baf6502cbe0ece6393d9e3bc37a5166
Dominik Schratl <hi@dominik-schratl.at>
2024-07-31 07:12:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Praktikum 2024 - Demo-Projekt
+
+Run the tailwind watcher to recompile on CSS changes
+```
+npm run dev:css
+```
+
+Start the development server on localhost:8080
+```
+npm run develop
+```
+
+Compile minified CSS
+```
+npm run build:css
+``
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'brand-green': '#585943',
+      'brand-green-light': 'rgba(88, 89, 67, 1)',
+      'brand-yellow': 'rgba(211, 162, 0)',
+      'brand-yellow-light': 'rgba(211, 162, 0, 1)',
+      'brand-black': '#000000',
+      'brand-white': '#ffffff',
+    },
+    fontFamily: {
+			'sans': ['Quicksand', 'sans-serif'],
+			'serif': ['DM Serif Display', 'serif'],
+      'hand': ['Arial', 'sans'], //TODO
+		},
+  },
+}
+
